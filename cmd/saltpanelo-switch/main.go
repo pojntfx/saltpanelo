3b14327dea7e411b911c096b9872f2cdefb53f16
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-12-20 23:56:51 +0100

    feat: Start implementation of RTT switch functionality and in-memory RTT database

--- cmd/saltpanelo-switch/main.go
+++ cmd/saltpanelo-switch/main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"time"
 
+	"github.com/pojntfx/dudirekta/pkg/rpc"
+	"github.com/pojntfx/saltpanelo/pkg/services"
+)
+
+func main() {
+	raddr := flag.String("raddr", "localhost:1337", "Remote address")
+	timeout := flag.Duration("timeout", time.Second*10, "Time after which to assume that a call has timed out")
+	verbose := flag.Bool("verbose", false, "Whether to enable verbose logging")
+
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	l := services.NewSwitch(*verbose)
+	clients := 0
+	registry := rpc.NewRegistry(
+		l,
+		services.RouterRemote{},
+		*timeout,
+		ctx,
+		&rpc.Options{
+			ResponseBufferLen: rpc.DefaultResponseBufferLen,
+			OnClientConnect: func(remoteID string) {
+				clients++
+
+				log.Printf("%v clients connected", clients)
+			},
+			OnClientDisconnect: func(remoteID string) {
+				clients--
+
+				log.Printf("%v clients connected", clients)
+			},
+		},
+	)
+	l.Peers = registry.Peers
+
+	conn, err := net.Dial("tcp", *raddr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	log.Println("Connected to", conn.RemoteAddr())
+
+	if err := registry.Link(conn); err != nil {
+		panic(err)
+	}
 }
