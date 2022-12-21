cabcce0b52fb7b0ab02030fbb90600951bbf5477
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-12-21 23:08:20 +0100

    refactor: Handle RPC registration once

--- pkg/services/router.go
+++ pkg/services/router.go
@@ -22,6 +22,10 @@ func HandleClientDisconnect(router *Router, remoteID string) {
 	router.onClientDisconnect(remoteID)
 }
 
+func HandleOpen(router *Router) {
+	router.onOpen()
+}
+
 type switchMetadata struct {
 	addr string
 	rtts map[string]time.Duration
@@ -66,6 +70,76 @@ func (r *Router) onClientDisconnect(remoteID string) {
 	}
 }
 
+func (r *Router) onOpen() {
+	t := time.NewTicker(r.rttTestInterval)
+	defer t.Stop()
+
+	for range t.C {
+		var wg sync.WaitGroup
+
+		for remoteID, peer := range r.Peers() {
+			wg.Add(1)
+
+			go func(remoteID string, peer SwitchRemote) {
+				defer wg.Done()
+
+				r.switchesLock.Lock()
+				addrs := []string{}
+				for id, sw := range r.switches {
+					// Don't test RTT to self
+					if id == remoteID {
+						continue
+					}
+
+					addrs = append(addrs, sw.addr)
+				}
+				r.switchesLock.Unlock()
+
+				if r.verbose {
+					log.Println("Starting RTT tests for switch with ID", remoteID)
+				}
+
+				rttTestResults, err := peer.TestRTT(nil, r.rttTestTimeout, addrs)
+				if err != nil {
+					log.Println("Could not run RTT test for switch with ID", remoteID, ", continuing:", err)
+
+					return
+				}
+
+				if len(rttTestResults) < len(addrs) {
+					log.Println("Received invalid length of RTT tests results from switch with ID", remoteID, ", continuing")
+
+					return
+				}
+
+				rtts := map[string]time.Duration{}
+				for i, addr := range addrs {
+					rtts[addr] = rttTestResults[i]
+				}
+
+				r.switchesLock.Lock()
+
+				sm, ok := r.switches[remoteID]
+				if !ok {
+					r.switchesLock.Unlock()
+
+					return
+				}
+
+				sm.rtts = rtts
+
+				r.switchesLock.Unlock()
+
+				if r.verbose {
+					log.Println("Finished RTT tests for switch with ID", remoteID, ":", sm.rtts)
+				}
+			}(remoteID, peer)
+		}
+
+		wg.Wait()
+	}
+}
+
 func (r *Router) RegisterSwitch(ctx context.Context, addr string) error {
 	remoteID := rpc.GetRemoteID(ctx)
 
@@ -84,82 +158,6 @@ func (r *Router) RegisterSwitch(ctx context.Context, addr string) error {
 	if r.verbose {
 		log.Println("Added switch with ID", remoteID, "from topology")
 	}
-
-	go func() {
-		t := time.NewTicker(r.rttTestInterval)
-		defer t.Stop()
-
-		for range t.C {
-			found := false
-
-			r.switchesLock.Lock()
-			addrs := []string{}
-			for id, sw := range r.switches {
-				// Don't test RTT to self
-				if id == remoteID {
-					continue
-				}
-
-				addrs = append(addrs, sw.addr)
-			}
-			r.switchesLock.Unlock()
-
-			for remoteID, peer := range r.Peers() {
-				if remoteID == rpc.GetRemoteID(ctx) {
-					found = true
-
-					if r.verbose {
-						log.Println("Starting RTT tests for switch with ID", remoteID)
-					}
-
-					rttTestResults, err := peer.TestRTT(ctx, r.rttTestTimeout, addrs)
-					if err != nil {
-						log.Println("Could not run RTT test for switch with ID", remoteID, ", continuing:", err)
-
-						break
-					}
-
-					if len(rttTestResults) < len(addrs) {
-						log.Println("Received invalid length of RTT tests results from switch with ID", remoteID, ", continuing")
-
-						break
-					}
-
-					rtts := map[string]time.Duration{}
-					for i, addr := range addrs {
-						rtts[addr] = rttTestResults[i]
-					}
-
-					r.switchesLock.Lock()
-
-					sm, ok := r.switches[remoteID]
-					if !ok {
-						found = false
-
-						r.switchesLock.Unlock()
-
-						break
-					}
-
-					sm.rtts = rtts
-
-					r.switchesLock.Unlock()
-
-					if r.verbose {
-						log.Println("Finished RTT tests for switch with ID", remoteID, ":", sm.rtts)
-					}
-
-					break
-				}
-			}
-
-			if !found && r.verbose {
-				log.Println("Stopping RTT tests for switch with ID", remoteID, "due to disconnect")
-
-				return
-			}
-		}
-	}()
 
 	return nil
 }
