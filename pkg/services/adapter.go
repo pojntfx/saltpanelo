030d863b813a738e46a5f7cd7fcbf804681a1ac1
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-01-08 23:55:23 +0100

    feat: Add client route provisioning support

--- pkg/services/adapter.go
+++ pkg/services/adapter.go
@@ -3,8 +3,10 @@ package services
 import (
 	"context"
 	"errors"
+	"io"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -26,16 +28,21 @@ func RequestCall(adapter *Adapter, dstID string) (bool, string, error) {
 
 type Adapter struct {
 	verbose bool
+	ahost   string
 
 	onRequestCall      func(ctx context.Context, srcID string) (bool, error)
 	onCallDisconnected func(ctx context.Context, routeID string) error
 	onHandleCall       func(ctx context.Context, routeID, raddr string) error
+
+	routes     map[string]connPair
+	routesLock sync.Mutex
 
 	Peers func() map[string]GatewayRemote
 }
 
 func NewAdapter(
 	verbose bool,
+	ahost string,
 
 	onRequestCall func(ctx context.Context, srcID string) (bool, error),
 	onCallDisconnected func(ctx context.Context, routeID string) error,
@@ -43,10 +50,13 @@ func NewAdapter(
 ) *Adapter {
 	return &Adapter{
 		verbose: verbose,
+		ahost:   ahost,
 
 		onRequestCall:      onRequestCall,
 		onCallDisconnected: onCallDisconnected,
 		onHandleCall:       onHandleCall,
+
+		routes: map[string]connPair{},
 	}
 }
 
@@ -98,11 +108,27 @@ func (a *Adapter) TestThroughput(ctx context.Context, timeout time.Duration, add
 }
 
 func (a *Adapter) UnprovisionRoute(ctx context.Context, routeID string) error {
+	a.routesLock.Lock()
+	defer a.routesLock.Unlock()
+
 	if a.verbose {
 		log.Println("Unprovisioning route with ID", routeID)
 	}
 
-	// TODO: Close locally provisioned connection
+	route, ok := a.routes[routeID]
+	if !ok {
+		return ErrRouteNotFound
+	}
+
+	if err := route.src.Close(); err != nil {
+		return err
+	}
+
+	if err := route.dst.Close(); err != nil {
+		return err
+	}
+
+	delete(a.routes, routeID)
 
 	return a.onCallDisconnected(ctx, routeID)
 }
@@ -112,13 +138,108 @@ func (a *Adapter) ProvisionRoute(ctx context.Context, routeID string, raddr stri
 		log.Println("Provisioning route with ID", routeID, "to raddr", raddr)
 	}
 
-	lis, err := net.Listen("tcp", "localhost:0")
+	var src net.Conn
+	var dst net.Conn
+
+	cp := connPair{}
+
+	ready := make(chan struct{})
+	errs := make(chan error)
+
+	conn, err := net.Dial("tcp", raddr)
 	if err != nil {
 		return err
 	}
-	defer lis.Close()
 
-	// TODO: Dial `raddr` and create two-way `io.Copy` goroutines
+	cp.src = conn
+	src = conn
+
+	go func() {
+		ready <- struct{}{}
+	}()
+
+	laddr, err := net.ResolveTCPAddr("tcp", a.ahost+":0")
+	if err != nil {
+		return err
+	}
+
+	lis, err := net.ListenTCP("tcp", laddr)
+	if err != nil {
+		return err
+	}
+	cp.dst = lis
+
+	go func() {
+		conn, err := lis.Accept()
+		if err != nil {
+			if a.verbose {
+				log.Println("Could not accept dst connection, stopping:", err)
+			}
+
+			errs <- err
+
+			return
+		}
+
+		dst = conn
+
+		ready <- struct{}{}
+	}()
+
+	go func() {
+		i := 0
+	l:
+		for {
+			select {
+			case <-ready:
+				i++
+
+				if i == 2 {
+					break l
+				}
+			case err = <-errs:
+				break l
+			}
+		}
+
+		if i < 2 {
+			if a.verbose {
+				log.Println("Could not accept src or dst connection, stopping:", err)
+			}
+		}
+
+		go func() {
+			defer func() {
+				err := recover()
+
+				if a.verbose {
+					log.Println("Could not copy from dst to src, stopping:", err)
+				}
+			}()
+
+			if _, err := io.Copy(src, dst); err != nil {
+				panic(err)
+			}
+		}()
+
+		go func() {
+			defer func() {
+				err := recover()
+
+				if a.verbose {
+					log.Println("Could not copy from src to dst, stopping:", err)
+				}
+			}()
+
+			if _, err := io.Copy(dst, src); err != nil {
+				panic(err)
+			}
+		}()
+	}()
+
+	a.routesLock.Lock()
+	a.routes[routeID] = cp
+	a.routesLock.Unlock()
 
 	return a.onHandleCall(ctx, routeID, lis.Addr().String())
 }
